23e31f413cefa68ab2467828e5d9ffbd932bce1f
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2023-09-16 10:42:20 +0000

    route setup

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from .views import PersonListCreateView, PersonRetrieveUpdateDeleteView
 
 # Create a router for PersonViewSet
 urlpatterns = [
-    path('', PersonListCreateView.as_view(), name='person-list-create'),
-    path('<int:id>/', PersonRetrieveUpdateDeleteView.as_view(), name='person-retrieve-update-delete'),
+    path('api', PersonListCreateView.as_view(), name='person-list-create'),
+    path('api/<int:id>', PersonRetrieveUpdateDeleteView.as_view(), name='person-retrieve-update-delete'),
 ]
